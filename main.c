c82750020ac7c837f11ea23e3d58f8cf18797b5a
Mathew O'Donnell <c3150543@uon.edu.au>
2019-04-05 03:29:20 +0000

    Rotation Cyper ish

--- main.c
+++ main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
+     
+    char inputMessage[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    //char outputMessage[];
+    int i;
+    int key = 1;
+    
+ 
+             
+            for(i = 0; i < strlen(inputMessage); i++){
+                
+                if(inputMessage[i] >= 'Z'){
+                    
+                    inputMessage[i] = (inputMessage[i] + key) - 26;
+                    
+                    }
+                else{
+                    
+                     inputMessage[i] = inputMessage[i] + key;
+                     }
+                     
+                 }
+                 
+    printf("%s", inputMessage);
 
 return 0;
 }

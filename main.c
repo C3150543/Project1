791dda3060bcecbdbc4eb508d8143c9e3e68bf7a
Mathew O'Donnell <c3150543@uon.edu.au>
2019-04-12 03:49:55 +0000

    Rotation Function

--- main.c
+++ main.c
@@ -1,40 +1,91 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-int main() {
-     
-    char inputMessage[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ abcde";
-    int i;
-    int key = 1;
-    int checkPositive;
-    
+ 
 
-    for(i = 0; i < strlen(inputMessage); i++){
-        
-             if(inputMessage[i] >= 'a' && inputMessage[i] <= 'z')
-        {
-            inputMessage[i] = inputMessage[i] - 32;            
-        }
-        
-        checkPositive = inputMessage[i] + key - 65;
-        
-        if (checkPositive < 0){
-            
-            checkPositive = checkPositive +26;
-        }
-        
-   
-        
-        if(inputMessage[i] >= 'A' && inputMessage[i] <= 'Z') //Excluding all characters
-        {                                                    //that are not Capitals
-            
-            inputMessage[i] = (checkPositive % 26 ) + 65;
-            }
-            
-        }
-        
+int rotationCypherEncrypt(char *, int);
+
+ 
+
+int main() {
+
+    char inputMessage[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ abcde";
+
+ 
+
+              if(rotationCypherEncrypt(inputMessage, 15) < 0) {
+
+                             printf("Failed to encrypt file");
+
+                             exit(-1);
+
+              }
+
+       
+
     printf("%s", inputMessage);
-    
-    return 0;
-    
-    }
+
+   
+
+    return 0;   
+
+}
+
+ 
+
+int rotationCypherEncrypt(char *inputMessage, int key)
+
+{
+
+      int i;
+
+      int checkPositive;
+
+ 
+
+                key = key % 26;
+
+ 
+
+                for(i = 0; i < strlen(inputMessage); i++){
+
+        if(inputMessage[i] >= 'a' && inputMessage[i] <= 'z')
+
+        {
+
+            inputMessage[i] = inputMessage[i] - 32;           
+
+        }
+
+       
+
+        checkPositive = inputMessage[i] + key - 65;
+
+       
+
+        if (checkPositive < 0){
+
+           
+
+            checkPositive = checkPositive +26;
+
+        }
+
+       
+
+        if(inputMessage[i] >= 'A' && inputMessage[i] <= 'Z') //Excluding all characters
+
+        {                                                    //that are not Capitals  
+
+            inputMessage[i] = (checkPositive % 26 ) + 65;
+
+        }     
+
+      }
+
+ 
+
+              return 0;
+
+}
